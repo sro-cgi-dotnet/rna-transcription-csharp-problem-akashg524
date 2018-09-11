b2607153c1efef0bf41bfb51324c4ba33c802328
akashg524 <akashg524@gmail.com>
2018-09-11 14:45:31 +0530

    completed

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -6,7 +6,23 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string input="";
+            foreach(char ch in nucleotide)
+            {
+                switch (ch)
+                {
+                    case 'G' : input +='C';
+                    break;
+                    case 'C' : input +='G';
+                    break;
+                    case 'T' : input +='A';
+                    break;
+                    case 'A' : input +='U';
+                    break;
+                    default: break;
+                }
+            }
+            return input;
         }
     }
 }
